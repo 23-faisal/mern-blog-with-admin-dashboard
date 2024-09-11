4c4260959b62017245b517ba485b99432338eeff
23-faisal <a.faisal23r@gmail.com>
2024-09-11 10:57:58 +0600

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import { app } from "./app.js";
+import "dotenv/config";
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App is running at port: ${port}`);
+});
